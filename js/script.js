f7b876071648532a091167d372c3aa9868b7f457
Rzheznikova <rzheznikovajane@gmail.com>
2024-08-03 14:57:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     const canvas = document.getElementById('glCanvas');
-    // Устанавливаем размер canvas согласно размеру вьюпорта
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -11,33 +10,26 @@ window.onload = function() {
         return;
     }
 
-    // Настройка цвета очистки на белый
     gl.clearColor(1.0, 1.0, 1.0, 1.0);  // Очищаем белым цветом
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Подключаем слайдеры
     const horizontalSlider = document.getElementById('horizontalRangeSlider');
     const verticalSlider = document.getElementById('verticalRangeSlider');
-
-    // Контейнер для изображения
     const imageContainer = document.getElementById('imageContainer');
     const randomImage = document.getElementById('randomImage');
 
-    // Функция для загрузки JSON файла
     function loadImages(callback) {
-        fetch('images.json')
+        fetch('goticheskaya/images.json')
             .then(response => response.json())
             .then(data => callback(data))
             .catch(error => console.error('Error loading images:', error));
     }
 
-    // Функция для выбора случайного изображения
     function getRandomImage(images) {
         const randomIndex = Math.floor(Math.random() * images.length);
         return images[randomIndex];
     }
 
-    // Обработка остановки горизонтального слайдера
     horizontalSlider.addEventListener('change', function() {
         loadImages(function(images) {
             const randomImagePath = getRandomImage(images);
@@ -46,9 +38,8 @@ window.onload = function() {
         });
     });
 
-    // Обработка движения вертикального слайдера
     verticalSlider.addEventListener('input', function() {
         console.log('Vertical slider value:', this.value);
-        // Дополнительная логика для WebGL (если необходимо)
     });
 };
+
