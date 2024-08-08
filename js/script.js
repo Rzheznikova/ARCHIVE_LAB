31caf5304ecbdbf90913ef97017f4b42e015119d
Rzheznikova <rzheznikovajane@gmail.com>
2024-08-08 23:03:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,12 +15,6 @@ window.onload = function() {
 
     const imageContainer = document.getElementById('imageContainer');
     const randomImage = document.getElementById('randomImage');
-    const audioPlayer = document.getElementById('audioPlayer');
-    let audioFiles = [];
-    let currentAudioIndex = -1;
-    let audioEnabled = false;
-    let hasInteracted = false; // Флаг для проверки первого взаимодействия
-    let playPromise;
 
     function loadImages(callback) {
         fetch('images.json')
@@ -37,147 +31,44 @@ window.onload = function() {
             });
     }
 
-    function loadAudioFiles(callback) {
-        fetch('baseaudio.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => callback(data))
-            .catch(error => {
-                console.error('Error loading audio files:', error);
-                alert('Error loading baseaudio.json: ' + error.message);
-            });
-    }
-
-    function getRandomElement(arr) {
-        const randomIndex = Math.floor(Math.random() * arr.length);
-        return arr[randomIndex];
+    function getRandomImage(images) {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
     }
 
     function displayRandomImage() {
         loadImages(function(images) {
-            const randomImagePath = goticheskaya/${getRandomElement(images)};
+            const randomImagePath = `goticheskaya/${getRandomImage(images)}`;
             randomImage.src = randomImagePath;
             imageContainer.style.display = 'block';
         });
     }
 
-    function playRandomAudio() {
-        if (audioFiles.length === 0) return;
-
-        currentAudioIndex = Math.floor(Math.random() * audioFiles.length);
-        const randomAudioPath = baseaudio/${audioFiles[currentAudioIndex]};
-        console.log(Playing random audio: ${randomAudioPath});
-        audioPlayer.src = randomAudioPath;
-
-        playPromise = audioPlayer.play();
-
-        if (playPromise !== undefined) {
-            playPromise.then(() => {
-                console.log('Audio is playing');
-            }).catch(error => {
-                console.error('Error playing audio:', error);
-                // Handle the error here if necessary
-            });
-        }
-    }
-
-    function stopAudio() {
-        if (playPromise !== undefined) {
-            playPromise.then(() => {
-                console.log('Stopping audio');
-                audioPlayer.pause();
-                audioPlayer.currentTime = 0;
-            }).catch(error => {
-                console.error('Error stopping audio:', error);
-            });
-        }
-    }
-
-    function handleSliderMovement(slider, isVertical) {
-        const currentValue = parseInt(slider.value);
-        clearTimeout(stopTimer);
-
-        if (!isVertical) {
-            imageContainer.style.display = 'none';
-
-            stopTimer = setTimeout(() => {
-                if (currentValue === previousValue) { // Ползунок остановился
-                    displayRandomImage();
-                }
-            }, 500); // Проверка через 500 мс
-
-            previousValue = currentValue;
-        } else if (audioEnabled) {
-            if (currentValue !== previousValue) {
-                stopAudio();
-            }
-
-            stopTimer = setTimeout(() => {
-                if (currentValue === previousValue) { // Ползунок остановился
-                    playRandomAudio();
-                }
-            }, 500);
-
-            previousValue = currentValue;
-        }
-    }
-
     let previousValue = 0;
     let stopTimer;
 
+    function handleSliderMovement(slider) {
+        const currentValue = parseInt(slider.value);
+
+        clearTimeout(stopTimer);
+
+        stopTimer = setTimeout(() => {
+            displayRandomImage();
+        }, 500); // Проверка через 500 мс
+
+        previousValue = currentValue;
+    }
+
     const horizontalSlider = document.getElementById('horizontalRangeSlider');
     horizontalSlider.addEventListener('input', function() {
-        if (!hasInteracted) {
-            playRandomAudio();
-            hasInteracted = true;
-        }
-        handleSliderMovement(this, false);
+        handleSliderMovement(this);
     });
 
     const verticalSlider = document.getElementById('verticalRangeSlider');
     verticalSlider.addEventListener('input', function() {
-        if (!hasInteracted) {
-            playRandomAudio();
-            hasInteracted = true;
-        }
-        handleSliderMovement(this, true);
-    });
-
-    verticalSlider.addEventListener('mousedown', function(event) {
-        if (event.button === 0) {  // Левая кнопка мыши
-            console.log('Vertical slider clicked');
-            audioEnabled = true;
-            if (!hasInteracted) {
-                playRandomAudio();
-                hasInteracted = true;
-            }
-        }
-    });
-
-    verticalSlider.addEventListener('mouseup', function(event) {
-        if (event.button === 0) {  // Левая кнопка мыши
-            console.log('Vertical slider released');
-            handleSliderMovement(this, true);
-        }
-    });
-
-    verticalSlider.addEventListener('mousemove', function(event) {
-        if (audioEnabled) {
-            console.log('Vertical slider moved');
-            handleSliderMovement(this, true);
-        }
+        handleSliderMovement(this);
     });
 
     // Загружаем случайное изображение при загрузке страницы
     displayRandomImage();
-
-    // Загружаем список аудиофайлов при загрузке страницы
-    loadAudioFiles(function(files) {
-        audioFiles = files;
-        console.log('Loaded audio files:', audioFiles);
-    });
 };
