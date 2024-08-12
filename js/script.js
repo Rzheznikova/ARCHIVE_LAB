6ef042d50454d73f93fca6ecd18c787c4fd0df91
Rzheznikova <rzheznikovajane@gmail.com>
2024-08-12 22:58:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -259,7 +259,7 @@ window.onload = function() {
         // Позиционируем по оси Y для всех подписей
         const sliderBottom = sliderRect.bottom;
         const windowHeight = window.innerHeight;
-        const captionY = (windowHeight + sliderBottom) / 2; // середина между слайдером и низом экрана
+        const captionY = (windowHeight + sliderBottom) / 3; // середина между слайдером и низом экрана
 
         captionGoticheskaya.style.top = `${captionY}px`;
         captionMasterskaya.style.top = `${captionY}px`;
